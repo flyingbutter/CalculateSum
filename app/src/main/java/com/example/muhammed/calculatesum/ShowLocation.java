ea0bbac6a89113ef13123f4ae7aac57b980a5335
flyingbutter <mtanir22@gmail.com>
2017-06-09 17:04:25 +0300

    Calculate Sum V3

--- app/src/main/java/com/example/muhammed/calculatesum/ShowLocation.java
+++ app/src/main/java/com/example/muhammed/calculatesum/ShowLocation.java
@@ -1,13 +1,18 @@
 package com.example.muhammed.calculatesum;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,20 +25,22 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 
-public class ShowLocation extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+public class ShowLocation extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-  //  private final String TAG = "ravimaps";
+    //  private final String TAG = "ravimaps";
     private LocationManager locationManager;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
     TextView lats;
     TextView longs;
     private Location mLastLocation;
+    Handler h;
+    Runnable myRunnable;
 
     LocationListener mlocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            mLastLocation=location;
+            mLastLocation = location;
 
         }
 
@@ -60,6 +67,8 @@ public class ShowLocation extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_location);
+
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
@@ -67,11 +76,14 @@ public class ShowLocation extends AppCompatActivity implements GoogleApiClient.C
                 .build();
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        }
 
-
-         lats = (TextView) findViewById(R.id.textView8);
-         longs = (TextView) findViewById(R.id.textView9);
+        lats = (TextView) findViewById(R.id.textView8);
+        longs = (TextView) findViewById(R.id.textView9);
     }
+
     @Override
     public void onConnected(Bundle bundle) {
         mLocationRequest = LocationRequest.create();
@@ -82,28 +94,35 @@ public class ShowLocation extends AppCompatActivity implements GoogleApiClient.C
                 == PackageManager.PERMISSION_GRANTED) {
             float LOCATION_REFRESH_DISTANCE = 0;
             long LOCATION_REFRESH_TIME = 5;
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE,  mlocationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mlocationListener);
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if(mLastLocation!=null){
-               // Toast.makeText(getApplicationContext(), "YES! mLastLocation!=null", Toast.LENGTH_SHORT).show();
-              //  Log.i(TAG, mLastLocation.toString());
+            if (mLastLocation != null) {
+                // Toast.makeText(getApplicationContext(), "YES! mLastLocation!=null", Toast.LENGTH_SHORT).show();
+                //  Log.i(TAG, mLastLocation.toString());
 
-                lats.setText("Latitude:"+Double.toString(mLastLocation.getLatitude()));
-                longs.setText("Longitude"+Double.toString(mLastLocation.getLongitude()));
-
-                final Handler h = new Handler();
+                lats.setText("Latitude:" + Double.toString(mLastLocation.getLatitude()));
+                longs.setText("Longitude:" + Double.toString(mLastLocation.getLongitude()));
+                  h = new Handler();
                 final int delay = 10000; //milliseconds
 
-                h.postDelayed(new Runnable(){
-                    public void run(){
+                h.postDelayed(myRunnable= new Runnable() {
+
+                    public void run() {
+                        if (ActivityCompat.checkSelfPermission(getWindow().findViewById(android.R.id.content).getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getWindow().findViewById(android.R.id.content).getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                            //ActivityCompat.requestPermissions(,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+                            return;
+                        }
+                        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
                         lats.setText("Latitude:"+Double.toString(mLastLocation.getLatitude()));
                         longs.setText("Longitude"+Double.toString(mLastLocation.getLongitude()));
                         h.postDelayed(this, delay);
                     }
                 }, delay);
-                double latitude = mLastLocation.getLatitude();  //Save latitude in a double variable
-                double longitude = mLastLocation.getLongitude(); //Save longitude in a double variable
+               // double latitude = mLastLocation.getLatitude();  //Save latitude in a double variable
+               // double longitude = mLastLocation.getLongitude(); //Save longitude in a double variable
                 //Toast to display Coordinates
               //  Toast.makeText(getApplicationContext(), "Latitude = " + latitude + "\nLongitude = " + longitude, Toast.LENGTH_SHORT).show();
 
@@ -111,6 +130,15 @@ public class ShowLocation extends AppCompatActivity implements GoogleApiClient.C
         }
 
     }
+    @Override
+    public void onBackPressed() {
+        h.removeCallbacksAndMessages(null);
+
+        super.onBackPressed();
+    }
+
+
+
     @Override
     public void onLocationChanged(Location location) {
 
@@ -152,7 +180,28 @@ public class ShowLocation extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onStop() {
         mGoogleApiClient.disconnect();
+        h.removeCallbacks(myRunnable);
+
         super.onStop();
+
     }
 
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
